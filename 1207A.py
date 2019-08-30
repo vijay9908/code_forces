731a9e64c730a57abea11285fea9d55a35d0d608
vijay9908 <vijaytanmay055@gmail.com>
2019-08-31 00:50:46 +0530

    edits

--- 1207A.py
+++ 1207A.py
@@ -2,5 +2,9 @@ n = int(input())
 for i in range(n):
     b , p , f = map(int,input().split())
     h , c = map(int,input().split())
-    print(min(b//2,f)*c + max(0,min((b-(f*2))//2,p)*h));
-    
+    if(c > h):
+        print(min(b//2,f)*c + max(0,min((b-(f*2))//2,p)*h));
+    elif(b <= 1):
+        print("0")
+    else:
+        print(min(b//2,p)*h + max(0,min((b-(p*2))//2,f)*c))
